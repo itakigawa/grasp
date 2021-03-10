a64e09197621eb962f9077d52f788d51adc6f7f7
Ichigaku Takigawa <ichigaku.takigawa@riken.jp>
2021-03-10 18:27:11 +0900

    update

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,15 +1,30 @@
+if [ ! -d data ]; then
+  echo "retrieving data"
+  curl -OL https://www.dropbox.com/s/3r6ru1ww6evkw4w/grasp_data.tar.bz2
+  tar xvjf grasp_data.tar.bz2
+  rm -f grasp_data.tar.bz2
+fi
+
 echo "run example-1 (grasp)"
-mkdir output
-../bin/grasp -m 200 -n 1000 -o output ../data/test.sdf ../data/test.fasta ../data/test.interaction 
+if [ ! -d output ]; then
+  mkdir output
+fi
+
+DATA=data/samples
+
+../bin/grasp -m 200 -n 1000 -o output ${DATA}/test.sdf ${DATA}/test.fasta ${DATA}/test.interaction 
 
 echo "run example-2 (finder)"
-mkdir finder_out
-../bin/finder -o ./finder_out output/pair.patterns output/atom.dict ../data/test.sdf ../data/test.fasta ../data/test.interaction
+if [ ! -d finder_out ]; then
+  mkdir finder_out
+fi
+
+../bin/finder -o ./finder_out output/pair.patterns output/atom.dict ${DATA}/test.sdf ${DATA}/test.fasta ${DATA}/test.interaction
 
 echo "run example-3 (evaluator)"
-../bin/evaluator -o evaluate.dat output/pair.patterns output/atom.dict ../data/random.sdf ../data/random.fasta ../data/random.pairs finder_out/feat.table
+../bin/evaluator -o evaluate.dat output/pair.patterns output/atom.dict ${DATA}/random.sdf ${DATA}/random.fasta ${DATA}/random.pairs finder_out/feat.table
 
 echo "run example-4 (predictor)"
-../bin/predictor -n 100 -o predict.dat output/pair.patterns output/atom.dict ../data/random.sdf ../data/random.fasta finder_out/feat.table
+../bin/predictor -n 100 -o predict.dat output/pair.patterns output/atom.dict ${DATA}/random.sdf ${DATA}/random.fasta finder_out/feat.table
 
 
